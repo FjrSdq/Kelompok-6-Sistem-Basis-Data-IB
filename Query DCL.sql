36e60e511c389a54faab9b1ed19c9a4c40a6bbbc
FjrSdq <92590349+FjrSdq@users.noreply.github.com>
2022-10-10 20:34:19 +0800

    Penambahan User dan perbaikan syntax

--- Query DCL.sql
+++ Query DCL.sql
@@ -1,11 +1,12 @@
 -- Query DCL -- 
 
 # create user
-CREATE USER 'kelompok6'@'localhost' IDENTIFIED BY 'klp06';
+CREATE USER 'admin'@'localhost' IDENTIFIED BY klp_6;
+CREATE USER 'member'@'localhost' IDENTIFIED BY klpk_6;
 
+# give privilege to admin and student
+GRANT ALL PRIVILEGES ON klpk_6 TO 'admin'@'localhost'; 
+GRANT SELECT ON klpk_6 TO 'member'@'localhost';
 
-# give privilege to kelompok-6
-GRANT ALL PRIVILEGES ON klpk_6 TO 'kelompok6'@'localhost'; 
-
-# remove privilege create and drop from kelompok-6
-REVOKE CREATE, DROP ON klpk_6 FROM 'kelompok6'@'localhost';
+# remove privilege create and drop from admin
+REVOKE CREATE, DROP ON klpk_6 FROM 'admin'@'localhost';
