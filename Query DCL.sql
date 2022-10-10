533ab78d93f4424f620c293323718a5b045ba99a
Hafieddz <hasmy41@gmail.com>
2022-10-10 21:45:45 +0800

    update dcl

--- Query DCL.sql
+++ Query DCL.sql
@@ -1,25 +1,21 @@
 -- Query DCL -- 
 
 # create user
-CREATE USER 'kelompok6'@'localhost' IDENTIFIED BY 'klp06';
-CREATE USER 'admin'@'localhost' IDENTIFIED BY '1234';
-CREATE USER 'member'@'localhost' IDENTIFIED BY '5555';
+CREATE USER 'Dosen'@'localhost' IDENTIFIED BY 'klp06';
+CREATE USER 'Ketua'@'localhost' IDENTIFIED BY 'useyourmind';
 
 
 # give privilege to kelompok-6
-GRANT ALL PRIVILEGES ON klpk_6 TO 'kelompok6'@'localhost'; 
+GRANT ALL PRIVILEGES ON klpk_6 TO 'Dosen'@'localhost'; 
 
-#give privilege to admin and member
-GRANT ALL PRIVILEGES ON klpk_6 TO 'member'@'localhost';
-GRANT ALL PRIVILEGES ON klpk_6 TO 'admin'@'localhost';
+#give privilege to ketua
+GRANT ALL PRIVILEGES ON klpk_6 TO 'Ketua'@'localhost';
 
 # show status
 SHOW GRANTS FOR 'kelompok6'@'localhost';
 
-# remove privilege create and drop from kelompok-6
-REVOKE CREATE, DROP ON klpk_6 FROM 'member'@'localhost';
-REVOKE CREATE, DROP ON klpk_6 FROM 'admin'@'localhost';
+# remove privilege create and drop from ketua
+REVOKE CREATE, DROP, DELETE ON klpk_6 FROM 'Ketua'@'localhost';
 
 #show status
-SHOW GRANTS FOR 'admin'@'localhost';
-SHOW GRANTS FOR 'member'@'localhost';
+SHOW GRANTS FOR 'Ketua'@'localhost';
