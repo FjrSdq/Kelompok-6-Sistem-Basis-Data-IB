941554fe0f1416c37a923dd8593902ff126136d1
Hafieddz <hasmy41@gmail.com>
2022-10-10 20:42:44 +0800

    update dcl

--- Query DCL.sql
+++ Query DCL.sql
@@ -7,5 +7,8 @@ CREATE USER 'kelompok6'@'localhost' IDENTIFIED BY 'klp06';
 # give privilege to kelompok-6
 GRANT ALL PRIVILEGES ON klpk_6 TO 'kelompok6'@'localhost'; 
 
+# show status
+SHOW GRANTS FOR 'kelompok6'@'localhost';
+
 # remove privilege create and drop from kelompok-6
 REVOKE CREATE, DROP ON klpk_6 FROM 'kelompok6'@'localhost';
